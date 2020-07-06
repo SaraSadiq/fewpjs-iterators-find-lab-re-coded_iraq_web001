c31cb58cf762eb52a13d8db42a942f3655f337a5
Sara Nooraldin <sarasaadiq98@gmail.com>
2020-07-06 14:44:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,12 +3,7 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
-const record = [
-  {year: "2018", result: "L"},
-  {year: "2017", result: "W"},
-  {year: "2016", result: "N/A"}
-  //...
-]
+
 superbowlWin = (record)=>{
  let win =  record.find(record => record.win === "W" )
 return !!win ? win.year : 1969
